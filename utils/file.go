a03992c0f7706198eef5ab78fc1565a5aca6ad75
Shinohara Haruna <Zhu-Zhe-Yong@live.com>
2025-04-21 21:17:22 +0800

    🔥chore: Remove unused file and string utility functions and tests

--- utils/file.go
+++ utils/file.go
@@ -12,13 +12,3 @@ func FileExist(path string) bool {
 	}
 	return false // By default, when encountering other errors
 }
-
-// FilesExist 检查给定的文件路径是否都存在
-func FilesExist(paths ...string) bool {
-	for _, path := range paths {
-		if _, err := os.Stat(path); os.IsNotExist(err) {
-			return false
-		}
-	}
-	return true
-}
